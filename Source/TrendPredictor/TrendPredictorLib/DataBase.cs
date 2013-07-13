8e48434cdc87b5bc8319deda2de2d0675117f6b9
Spawek <spawnerer@gmail.com>
2013-07-14 00:16:07 +0200

    Added DataBase class implementation (with UTs)

---
+++ Source/TrendPredictor/TrendPredictorLib/DataBase.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendPredictorLib
+{
+    public class DataBase
+    {
+        private Dictionary<string, List<double>> db;
+
+        public DataBase()
+        {
+            db = new Dictionary<string, List<double>>();
+        }
+
+        public List<double> this[string key]
+        {
+            get
+            {
+                if (!db.ContainsKey(key))
+                {
+                    db[key] = new List<double>();
+                }
+
+                return db[key];
+            }
+
+            set { db[key] = value; }
+        }
+
+        public int CountVectorElements
+        { 
+            get 
+            { 
+                return ValidateElementsNoInVectors(); 
+            } 
+        }
+
+        public DataBase CloneDbRange(int beginIndex, int count)
+        {
+            int dataVectorSize = CountVectorElements;
+            if (beginIndex < 0 || beginIndex >= dataVectorSize)
+                throw new ArgumentOutOfRangeException("beginIndex");
+            if(count < 1)
+                throw new ArgumentOutOfRangeException("count");
+            if (beginIndex + count > dataVectorSize)
+                throw new ArgumentOutOfRangeException("beginIndex + count");
+
+            DataBase dataBase = new DataBase();
+            foreach (var item in db)
+            {
+                dataBase[item.Key] = item.Value.GetRange(beginIndex, count);
+            }
+
+            return dataBase;
+        }
+
+        //TODO: method should not throw exceptions as in it usuall workflow,
+        //EXPLANATION: if anything goes wrong in data validation, it means that
+        //             something went very wrong and program should stop it flow anyway
+        //             throwing an exception gives better debug possibility than just 
+        //             returning false (and some msg can be easily passed)
+        public void ValidateDB() 
+        {
+            if (db.Count < 0)
+                throw new ApplicationException("db is empty");
+
+            ValidateElementsNoInVectors();
+            ValidateElementsCorrectnessInVectors();
+        }
+
+        private void ValidateElementsCorrectnessInVectors()
+        {
+            foreach (var record in db)
+            {
+                foreach (var item in record.Value)
+                {
+                    if (Double.IsNaN(item) ||
+                        Double.IsInfinity(item))
+                    {
+                        throw new ApplicationException(
+                            String.Format("wrong value: {0} in doule value in vector: {1}",
+                                item.ToString(),
+                                record.Key));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>no of elements in each vector</returns>
+        private int ValidateElementsNoInVectors()
+        {
+            //all vectors should have same no of elements
+            int expectedElementsNo = db.First(x => true).Value.Count;
+
+            foreach (var item in db)
+            {
+                if (item.Value.Count != expectedElementsNo)
+                {
+                    throw new ApplicationException(
+                        String.Format("wrong no of elements in {0}: {1} instead of {2}",
+                            item.Key,
+                            item.Value.Count(),
+                            expectedElementsNo));
+                }
+            }
+
+            return expectedElementsNo;
+        }
+    }
+}
---
+++ Source/TrendPredictor/TrendPredictorLibUnitTests/DataBaseTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrendPredictorLib;
+
+namespace TrendPredictorLibUnitTests
+{
+    [TestClass]
+    public class DataBaseTests
+    {
+        [TestMethod]
+        public void DataBaseAccessTest()
+        {
+            DataBase db = new DataBase();
+
+            const double val1 = 2.0d;
+            const double val2 = 3.0d;
+            const string key = "Dupa";
+
+            db[key].Add(val1);
+            db[key].Add(val2);
+
+            Assert.AreEqual(2, db[key].Count);
+            Assert.AreEqual(val1, db[key][0]);
+            Assert.AreEqual(val2, db[key][1]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void DataBaseValidationShouldFailOnFDiffNoOfParameters()
+        {
+            DataBase db = new DataBase();
+
+            db["a"].Add(1.0d);
+
+            db["b"].Add(1.0d);
+            db["b"].Add(2.0d);
+
+            db.ValidateDB(); //expect exception
+        }
+
+        [TestMethod]
+        public void DataBaseValidationShouldPasslOnCorrectNoOfParametes()
+        {
+            DataBase db = new DataBase();
+
+            db["a"].Add(1.0d);
+            db["a"].Add(2.0d);
+
+            db["b"].Add(1.0d);
+            db["b"].Add(2.0d);
+
+            db.ValidateDB(); //expect no exception
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void DataBaseValidationShouldFailOnNaNValueInsideDb()
+        {
+            DataBase db = new DataBase();
+            db["a"].Add(Double.NaN);
+
+            db.ValidateDB(); //expect exception
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void DataBaseValidationShouldFailOnInfValueInsideDb()
+        {
+            DataBase db = new DataBase();
+            db["a"].Add(Double.PositiveInfinity);
+
+            db.ValidateDB(); //expect exception
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void DataBaseValidationShouldFailOnMinusInfValueInsideDb()
+        {
+            DataBase db = new DataBase();
+            db["a"].Add(Double.NegativeInfinity);
+
+            db.ValidateDB(); //expect exception
+        }
+
+        [TestMethod]
+        public void DataBaseValidationShouldCountVectorElementsCorrectly()
+        {
+            DataBase db = new DataBase();
+
+            db["b"].Add(1.0d);
+            db["b"].Add(1.0d);
+
+            Assert.AreEqual(2, db.CountVectorElements);
+        }
+
+        [TestMethod]
+        public void DataBaseCloneRangeTest()
+        {
+            DataBase db = new DataBase();
+
+            db["a"].Add(1.0d);
+            db["a"].Add(2.0d);
+            db["a"].Add(3.0d);
+
+            db["b"].Add(11.0d);
+            db["b"].Add(12.0d);
+            db["b"].Add(13.0d);
+
+            
+            const int beginIndex = 1;
+            const int count = 2;
+            DataBase dbClone = db.CloneDbRange(beginIndex, count);
+
+            Assert.AreEqual(2, dbClone.CountVectorElements);
+            Assert.AreEqual(2.0d, dbClone["a"][0]);
+            Assert.AreEqual(3.0d, dbClone["a"][1]);
+            Assert.AreEqual(12.0d, dbClone["b"][0]);
+            Assert.AreEqual(13.0d, dbClone["b"][1]);
+        }
+
+    }
+}
