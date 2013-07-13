9022a2b43ec66afbadb4bb269a4e744ad47b9363
Spawek <spawnerer@gmail.com>
2013-07-14 00:30:02 +0200

    Added FileDataBaseProvider (with UT)

---
+++ Source/TrendPredictor/TrendPredictorLib/FileDataBaseProvider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendPredictorLib
+{
+    public class FileDataBaseProvider : DataBaseProvider
+    {
+        private string filePath;
+        private DataBase db = null;
+        private NumberFormatInfo doubleFormatProvider = new NumberFormatInfo();
+
+        public FileDataBaseProvider(string _filePath)
+        {
+            filePath = _filePath;
+
+            doubleFormatProvider.NumberDecimalSeparator = ".";
+        }
+
+        public DataBase GetDataBase()
+        {
+            if (db != null)
+                return db;
+
+            db = new DataBase();
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line = sr.ReadLine(); //1st line is not needed (for now)
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] splittedLine = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (splittedLine[0] == "5") //just take 5 - min data
+                    {
+                        db["open"].Add(Convert.ToDouble(splittedLine[4], doubleFormatProvider));
+                        db["high"].Add(Convert.ToDouble(splittedLine[2], doubleFormatProvider));
+                        db["low"].Add(Convert.ToDouble(splittedLine[3], doubleFormatProvider));
+                        db["close"].Add(Convert.ToDouble(splittedLine[5], doubleFormatProvider));
+                    }
+
+                }
+            }
+
+            return db;
+        }
+    }
+
+}
---
+++ Source/TrendPredictor/TrendPredictorLibUnitTests/FileDataBaseProviderTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrendPredictorLib;
+
+namespace TrendPredictorLibUnitTests
+{
+    [TestClass]
+    public class FileDataBaseProviderTests
+    {
+        [TestMethod]
+        public void FileDataBaseProviderTest()
+        {
+            DataBaseProvider dbProvider = new FileDataBaseProvider("../../../../../Data/EURUSD.csv");
+            var db = dbProvider.GetDataBase();
+
+            Assert.AreNotEqual(0, db.CountVectorElements);
+            //provider will throw exception in case of any problems so more assertions are not needed
+        }
+
+    }
+}
