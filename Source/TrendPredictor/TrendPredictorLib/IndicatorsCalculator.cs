2247aa88d7b8b560b16399e057004cad318e6ebf
Spawek <spawnerer@gmail.com>
2013-07-14 14:13:53 +0200

    added IndicatorsCalculator with changeIndicator and movingMeanIndicator (with UTs)

---
+++ Source/TrendPredictor/TrendPredictorLib/IndicatorsCalculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrendPredictorLib
+{
+	public static class IndicatorsCalculator
+	{
+		/// <summary>
+		/// its like change in percents, but here are no percents (no *100)
+		/// </summary>
+		public static List<double> CalculateChangeIndicator(List<double> inputVector)
+		{
+			if (inputVector.Count < 2)
+				throw new ArgumentException("input vector is too small");
+
+			List<double> output = new List<double>();
+			output.Add(double.NaN); //1st field cannot be calculated
+
+			for (int i = 1; i < inputVector.Count; i++)
+			{
+				double valueChange = inputVector[i] - inputVector[i-1];
+				output.Add(valueChange / inputVector[i-1]);
+			}
+
+			return output;
+		}
+	
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="inputVector"></param>
+		/// <param name="backstepsTaken">it includes current point, 1 would be just vector copy</param>
+		/// <returns></returns>
+		public static List<double> CalculateMovingMean(List<double> inputVector, int backstepsTaken)
+		{
+			if (backstepsTaken < 2)
+				throw new ArgumentOutOfRangeException("backstepsTaken");
+			if (inputVector.Count < backstepsTaken)
+				throw new ArgumentException("input vector is too small");
+
+			List<double> output = new List<double>();
+
+			for (int i = 0; i < backstepsTaken - 1; i++)
+			{
+				output.Add(double.NaN);
+			}
+
+			for (int i = backstepsTaken - 1; i < inputVector.Count; i++)
+			{
+				output.Add(inputVector.GetRange(i - backstepsTaken + 1, backstepsTaken).Sum()/backstepsTaken);
+			}
+
+			return output;
+		}
+	}
+}
---
+++ Source/TrendPredictor/TrendPredictorLibUnitTests/IndicatorsCalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrendPredictorLib;
+using System.Collections.Generic;
+
+namespace TrendPredictorLibUnitTests
+{
+    [TestClass]
+    public class IndicatorsCalculatorTests
+    {
+        List<double> smallTestData = new List<double>() { 1.0d, 2.0d, 3.0d, 4.0d, 5.0d, 6.0d };
+
+        [TestMethod]
+        public void IndicatorsCalculatorChangeIndicatorTest()
+        {
+            List<double> changeInd = IndicatorsCalculator.CalculateChangeIndicator(smallTestData);
+
+            Assert.AreEqual(smallTestData.Count, changeInd.Count);
+
+            double delta = 0.0001d;
+            Assert.AreEqual(double.NaN, changeInd[0]);
+            Assert.AreEqual(1.0d, changeInd[1], delta); //  100% change (1.0 -> 2.0) 
+            Assert.AreEqual(0.5d, changeInd[2], delta); //  50% change (2.0 -> 3.0)
+        }
+
+        [TestMethod]
+        public void IndicatorsCalculatorMovingMeanTest()
+        {
+            List<double> movingMeanInd = IndicatorsCalculator.CalculateMovingMean(smallTestData, 3);
+
+            Assert.AreEqual(smallTestData.Count, movingMeanInd.Count);
+
+            double delta = 0.0001d;
+            Assert.AreEqual(double.NaN, movingMeanInd[0]);
+            Assert.AreEqual(double.NaN, movingMeanInd[1]);
+            Assert.AreEqual(2.0d, movingMeanInd[2], delta); // (1+2+3)/3 = 2;
+            Assert.AreEqual(3.0d, movingMeanInd[3], delta); // (2+3+4)/3 = 2;
+        }
+
+    }
+}
