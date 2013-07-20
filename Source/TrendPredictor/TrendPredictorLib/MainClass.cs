fcd909d36dc49ba3d248915ba2f149a3bad90d0c
Spawek <spawnerer@gmail.com>
2013-07-20 14:34:50 +0200

    added main class (for test purposes)

---
+++ Source/TrendPredictor/TrendPredictorLib/MainClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendPredictorLib
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            FileDataBaseProvider dbProvider = new FileDataBaseProvider("../../../../../Data/EURUSD.csv");
+            var db = dbProvider.GetDataBase();
+            db.PrintDBProperties();
+            db += new DataBase(IndicatorsCalculator.CalculateChangeIndicator(db["open"]), "openChangeInd");
+            db.PrintDBProperties();
+
+
+            Console.WriteLine("Press any button");
+            Console.ReadKey();
+        }
+    }
+}
