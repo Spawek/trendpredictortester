f7e6017cffaa049b192405a75a1bab90d7aaf825
Spawek <spawnerer@gmail.com>
2013-07-22 23:56:23 +0200

    added Helpers class (with UT class) added MofifiedFibbonacciCalc to Helpers (with UT)

---
+++ Source/TrendPredictor/TrendPredictorLib/Helpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrendPredictorLib
+{
+    public class Helpers
+    {
+        private static List<int> ModFibbonacciDb = new List<int>() { 0, 1, 2 };
+        /// <summary>
+        /// Fibbonacci starting with 0,1,2
+        /// this foo is optimized to calc this only once
+        /// </summary>
+        /// <param name="index">index > 0</param>
+        /// <returns>0,1,2,3,5,8,13,21,...</returns>
+        public static int ModifiedFibbonacci(int index)
+        {
+            for (int i = ModFibbonacciDb.Count; i <= index; i++)
+            {
+                ModFibbonacciDb.Add(ModFibbonacciDb[i-1] + ModFibbonacciDb[i-2]);
+            }
+
+            return ModFibbonacciDb[index];
+        }
+    }
+}
---
+++ Source/TrendPredictor/TrendPredictorLibUnitTests/HelpersTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrendPredictorLib;
+
+namespace TrendPredictorLibUnitTests
+{
+    [TestClass]
+    public class HelpersTests
+    {
+        [TestMethod]
+        public void ModifiedFibbonacciTest()
+        {
+            Assert.AreEqual(0, Helpers.ModifiedFibbonacci(0));
+            Assert.AreEqual(1, Helpers.ModifiedFibbonacci(1));
+            Assert.AreEqual(2, Helpers.ModifiedFibbonacci(2));
+            Assert.AreEqual(3, Helpers.ModifiedFibbonacci(3));
+            Assert.AreEqual(5, Helpers.ModifiedFibbonacci(4));
+            Assert.AreEqual(8, Helpers.ModifiedFibbonacci(5));
+            Assert.AreEqual(13, Helpers.ModifiedFibbonacci(6));
+            Assert.AreEqual(21, Helpers.ModifiedFibbonacci(7));
+            Assert.AreEqual(34, Helpers.ModifiedFibbonacci(8));
+        }
+    }
+}
